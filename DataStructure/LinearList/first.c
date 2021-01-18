a1d05491fbd49db288fc8d0eee6b09080439c090
HIM <56875404+HIM@users.noreply.github.com>
2021-01-18 23:30:16 +0800

    first.c

---
+++ DataStructure/LinearList/first.c
@@ -0,0 +1,13 @@
+#include"stdio.h"
+#define LIST_INIT_SIZE 100
+#define LISTINCREMENT 10
+typedef struct{
+    ElemType *elem;
+    int length;
+    int listsize;
+}SqList;
+int main(){
+
+
+    return 0;
+}
