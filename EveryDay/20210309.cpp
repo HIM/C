0d62e6ee40f36cc670155ef51c86b69aa5383ac8
HIM <56875404+HIM@users.noreply.github.com>
2021-03-09 22:25:20 +0800

    39

---
+++ EveryDay/20210309.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+//递归
+long recurrence(int X){
+    int i=0;
+    long Z = 0;
+    while(X>0){
+        if(X<2){
+
+        }
+    }
+}
+
+int main(){
+    int X = 0;
+    printf("请输入一个整数：");
+    cin>>X;
+    printf("二进制数为：%ld\n",recurrence(X));
+    return 0;
+}
